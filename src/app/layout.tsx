768e1261a8b299f1d40bc4b0dfdfe6e19114b00f
Chanyoung Park <rhdwkrxla68@gmail.com>
2025-02-03 12:40:17 +0900

    Init: Initialized Nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import 'normalize.css';
+import '@/shared/styles/fonts.css';
+export const metadata: Metadata = {
+  title: 'Thumbnailer',
+  description: '기술 블로그에 사용할 썸네일을 제작하는 서비스입니다.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
